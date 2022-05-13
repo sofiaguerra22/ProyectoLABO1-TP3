33bc7b207c88c53a09e618ba0aec821efce6944c
lolanoval <lolanoval65@gmail.com>
2022-05-13 08:45:48 -0300

    enums

---
+++ TP3/Enum.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+enum class eOrgano {corazon, higado, pancreas, huesos,
+	rinion, pulmones, intestino, piel, corneas};
+enum class eTipoSangre {ON,OP,AN,AP,BN,BP,ABN,ABP};
+
+string OrganoToString(eOrgano organo);
+string TipoSangreToString(eTipoSangre sangre);
+int OrganoToInt(eOrgano organo);
+
